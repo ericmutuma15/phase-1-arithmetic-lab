645cae1e519d466afa8d96e58480e9a8d51582f2
eric mutuma <ericmutuma15@gmail.com>
2024-03-24 16:26:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+  
+const multiply = num1*num2
+
+
+
+
+let random = Math.floor((Math.random() * 100) + 1);
+
+const num3 = 11
+const num4 = 37
+const mod = num4 % num3 
+
+
+const x = [17, 12, 20, 5]
+const max = Math.max (...x)
